12788828bd91f4438198b9ea56fb79aaf5ea2379
Nick Earwood <nearwood@gmail.com>
2017-09-17 14:15:28 -0400

    fix account, add payment method, test buy ready.

--- index.js
+++ index.js
@@ -16,16 +16,42 @@ const pairSplit = pair.split('-');
 const cryptoCurrency = pairSplit[0];
 const fiatCurrency = pairSplit[1];
 
-const srcAccount = process.env.SOURCE_ACCOUNT;
+let paymentMethod = process.env.PAYMENT_METHOD;
+let account = process.env.ACCOUNT;
 
 (async () => {
     try {
-        if (!srcAccount) {
+        if (!account) {
             const accounts = await client.getAccounts();
             console.error("No account specified to use for buys. Available accounts:");
             for (let i = 0; i < accounts.length; ++i) {
-                console.dir(accounts[i]);
+                let a = accounts[i];
+                console.log(`Name: ${a.name}, primary: ${a.primary}, type: ${a.type}, currency: ${a.currency}, balance: ${a.balance.amount} id: ${a.id}`);
             }
+            return;
+        } else {
+            account = await client.getAccount(account);
+            console.log(`Using account: ${account.name} - ${account.balance.amount} ${account.balance.currency} (${account.native_balance.amount} ${account.native_balance.currency})`);
+        }
+
+        let paymentMethods = await client.getPaymentMethods();
+
+        if (!paymentMethod) {
+            console.error("No payment method specified to use for buys. Available methods:");
+        }
+
+        for (let i = 0; i < paymentMethods.length; ++i) {
+            let m = paymentMethods[i];
+            if (!paymentMethod) {
+                console.log(`Name: ${m.name}, type: ${m.type}, currency: ${m.currency}, id: ${m.id}`);
+            } else if (m.type == paymentMethod || m.id == paymentMethod) {
+                paymentMethod = m.id;
+                console.log(`Using payment method: ${m.type}, ${m.name}`);
+                //TODO Show balance if linked to account?
+            }
+        }
+
+        if (!paymentMethod) {
             return;
         }
 
@@ -36,16 +62,12 @@ const srcAccount = process.env.SOURCE_ACCOUNT;
         const volumeToBuy = (investmentAmount / price.amount).toFixed(6);
         const roundedInvestmentAmount = (volumeToBuy * price.amount).toFixed(3);
 
-        // Kraken does not allow to buy less than 0.002XBT
+        //TODO Check Coinbase minimums
         if (volumeToBuy < 0.002) {
-            console.log(`Increase your investment amount.`,
-                        `You must buy at least 0.002 ${cryptoCurrency} per trade`);
+            console.log(`Increase your investment amount. You must buy at least 0.002 ${cryptoCurrency} per trade`);
             return;
         }
-        const logMessage = util.format(`[${timestamp()}] Buying ${volumeToBuy} ${cryptoCurrency}`,
-                                       `which is equal to ${roundedInvestmentAmount} ${fiatCurrency}`,
-                                       `at price ${price.amount} ${fiatCurrency}/${cryptoCurrency}\n`);
-        
+        const logMessage = `[${timestamp()}] Buy ${volumeToBuy} ${cryptoCurrency} * ${price.amount} ${fiatCurrency}/${cryptoCurrency} = ${roundedInvestmentAmount} ${fiatCurrency}`;
         console.log(logMessage);
         fs.appendFile('buy.log', logMessage, err => {
             if (err) {
@@ -54,22 +76,15 @@ const srcAccount = process.env.SOURCE_ACCOUNT;
             }
         });
 
-		return;
-
-        var account = client.getAccount('');
-        const tradeResponse = await client.api('AddOrder', {
-            pair,
-            volume: volumeToBuy,
-            type: 'buy',
-            ordertype: 'market'
+        const tradeResponse = await account.buy({
+            amount: volumeToBuy,
+            currency: cryptoCurrency,
+            payment_method: paymentMethod,
+            quote: true
         });
-        // Retrieve and log transaction ids
-        const txIds = tradeResponse['result']['txid'];
-        if (typeof txIds === 'undefined') {
-            console.log('Unable to read transaction ids');
-            return;
-        }
-        console.log(util.format(`[${timestamp()}] Trade completed successfully: ${txIds}`));
+        
+        console.dir(tradeResponse);
+        //console.log(util.format(`[${timestamp()}] Trade completed successfully: ${txIds}`));
     } catch (e) {
         console.log(e);
         // Log to file in case of failure
